ad8bdb2fac04505d8a067db1b858dc0d2c8ffc0a
Christian Duffoo <cduffoo03@gmail.com>
2026-05-20 14:31:51 -0400

    Update ingest_keywords.py

--- ingest/keywords/ingest_keywords.py
+++ ingest/keywords/ingest_keywords.py
@@ -2,7 +2,6 @@ import os, sys, re
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType, StringType, ArrayType, StructField, StructType
 from pyspark.sql.functions import from_json, col, regexp_replace, when, concat, lit
-from json_repair import repair_json
 
 os.environ["PYSPARK_PYTHON"] = f"{sys.executable}"
 os.environ["PYSPARK_DRIVER_PYTHON"] = f"{sys.executable}"
@@ -17,7 +16,6 @@ def clean_csv(inputpath, outputpath):
     text = ''
     with open(inputpath, 'r', encoding='utf-8') as file:
         text = file.read()
-        text = repair_json(text)
 
     text = re.sub(r'""([^"]+)""', r"'\1'", text)
     with open(outputpath, 'w', encoding='utf-8') as file:

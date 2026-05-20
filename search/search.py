137c9e7e7e8be5328433dfed6469357e884e22d8
cduffoo03 <cduffoo03@gmail.com>
2026-05-20 15:37:39 -0400

    added search.py file

--- search/search.py
+++ search/search.py
@@ -2,26 +2,18 @@ import weaviate
 from weaviate.classes.query import MetadataQuery, Filter
 
 client = weaviate.connect_to_local()
-songs = client.collections.get("Movie")
+movies = client.collections.get("Movie")
 
-'''response = songs.query.near_text(
-    query="songs about love",
-    limit=5,
-    return_metadata=MetadataQuery(distance=True),
-)
-
-print("=== Heartbreak query ===")
-for obj in response.objects:
-    print(f"  {obj.properties['title']} — {obj.properties['artist']} (distance: {obj.metadata.distance:.3f})")
-'''
-
-#query = "Taylor Swift breakup songs"
 query = input("Search: ")
 
-for alpha in [1.0, 0.5, 0.0]:
-    print(f"\n=== Hybrid search, alpha={alpha} ===")
-    response = songs.query.hybrid(query=query, limit=5, alpha=alpha)
-    for obj in response.objects:
-        print(f"  {obj.properties['title']} — {obj.properties['artist']}")
+response = movies.query.hybrid(
+    query=query,
+    limit=5,
+    alpha=0.5,
+)
+
+for obj in response.objects:
+    print(f"{obj.properties['title']} - distance: {obj.metadata.distance:.3f}")
+
 
 client.close()
